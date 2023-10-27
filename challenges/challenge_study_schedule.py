a74f8dc1c5a5bf30730a1d1f987bffe6b9771918
Felipe Cardoso <felipecardoso@pop-os.localdomain>
2023-10-27 19:53:09 -0300

    Req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        count = 0
+        for beginning, finish in permanence_period:
+            if beginning <= target_time <= finish:
+                count += 1
+        return count
+    except TypeError:
+        return None
