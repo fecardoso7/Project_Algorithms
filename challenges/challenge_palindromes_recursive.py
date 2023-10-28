c9169e440524a959c188d2fb19504bcc28098bae
Felipe Cardoso <felipecardoso@pop-os.localdomain>
2023-10-27 22:41:14 -0300

    Ajuste comentários imcompletos

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,14 +1,17 @@
-# Função recursiva para verificar se uma palavra é um
-# palíndromo.
+# Função recursiva para verificar se uma palavra é um palíndromo.
+# Parâmetros:
+# - word: a palavra a ser verificada.
+# - low_index: índice inicial da palavra.
+# - high_index: índice final da palavra.
+# Retorna True se a palavra for um palíndromo, False caso contrário.
 def is_palindrome_recursive(word, low_index, high_index):
-    if not word:  # Se a palavra estiver vazia, não
+    if not word:  # Se a palavra estiver vazia, não é um palíndromo.
         return False
 
-    if low_index >= high_index:  # Se índices se
+    if low_index >= high_index:  # Se os índices se cruzarem, é um palíndromo.
         return True
 
-    if word[low_index] != word[high_index]:  # Se
+    if word[low_index] != word[high_index]:
         return False
 
-    # Chama recursivamente com índices alterados
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,11 @@
-# Função para calcular a quantidade de períodos de
-# permanência que incluem o horário alvo.
+# Função para calcular a quantidade de períodos de permanência
+# que incluem o horário alvo.
 def study_schedule(permanence_period, target_time):
     try:
-        count = 0  # Inicializa a contagem de períodos de
+        count = 0  # Inicializa a contagem de períodos inclusivos.
         for beginning, finish in permanence_period:
-            if beginning <= target_time <= finish:  # Verifica se
-                count += 1  # Incrementa a contagem se o período
-        return count  # Retorna o número de períodos que
+            if beginning <= target_time <= finish:
+                count += 1
+        return count  # Retorna o número de períodos que incluem o horário.
     except TypeError:
-        return None  # Retorna None se houver um erro de tipo
+        return None  # Retorna None se houver erro de tipo durante a execução.
