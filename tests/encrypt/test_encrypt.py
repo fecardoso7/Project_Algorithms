04bbf104006932b2d7602519f0486b50552368fe
Felipe Cardoso <felipecardoso@pop-os.localdomain>
2023-10-27 22:06:50 -0300

    Req 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message('message', 'str')
+
+    assert encrypt_message('maior', 10) == 'roiam'
+    assert encrypt_message('par', 2) == 'r_ap'
+    assert encrypt_message('impar', 3) == 'pmi_ra'
